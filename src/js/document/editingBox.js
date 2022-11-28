feca6cd895de5f4253cc28558d3459aa195d0331
Roman <r0m4uk0106@gmail.com>
2022-11-28 12:30:13 +0300

    Adding form: added checklist functional

--- src/js/document/editingBox.js
+++ src/js/document/editingBox.js
@@ -1,13 +1,17 @@
-import checklistButtonSvg from '../../assets/checklist-button.svg';
-import reminderButtonSvg from '../../assets/reminder-button.svg';
-import pinButtonSvg from '../../assets/pin-button.svg';
-import tagButtonSvg from '../../assets/tag-button.svg';
-import archiveButtonSvg from '../../assets/archive-button.svg';
-import trashButtonSvg from '../../assets/trash-button.svg';
+import checklistButtonSvg from 'assets/checklist-button.svg';
+import reminderButtonSvg from 'assets/reminder-button.svg';
+import pinButtonSvg from 'assets/pin-button.svg';
+import tagButtonSvg from 'assets/tag-button.svg';
+import archiveButtonSvg from 'assets/archive-button.svg';
+import trashButtonSvg from 'assets/trash-button.svg';
+import checklist from './checklist';
 
-const editingBox = {
-  editing: `
-  <div class="adding-form__editing-box editing-box">
+const editingBlock = {
+  block: document.createElement('div'),
+  createEditingBox() {
+    this.block.classList.add('adding-form__editing-box');
+    this.block.classList.add('editing-box');
+    this.block.innerHTML = `
     <ul class="editing-box__list editing-list">
       <li class="editing-list__item editing-item">
         <button class="editing-item__checklist-button checklist-button" type="button">
@@ -16,12 +20,12 @@ const editingBox = {
       </li>
       <li class="editing-list__item editing-item">
         <button class="editing-item__reminder-button reminder-button" type="button">
-         ${reminderButtonSvg}
-       </button>
+          ${reminderButtonSvg}
+        </button>
       </li>
       <li class="editing-list__item editing-item">
         <button class="editing-item__pin-button pin-button" type="button">
-         ${pinButtonSvg}
+          ${pinButtonSvg}
         </button>
       </li>
       <li class="editing-list__item editing-item">
@@ -41,9 +45,43 @@ const editingBox = {
       </li>
     </ul>
     <button class="editing-box__cancel-button" type="button">
-      Cancel
+      Close
     </button>
-  </div>`,
+    `;
+  },
+  changingToChecklist() {
+    const checklistButton = this.block.querySelector('.checklist-button');
+    const inputBox = document.querySelector('.input-box');
+    const textarea = document.querySelector('.input-box__description');
+    checklistButton.addEventListener('click', () => {
+      textarea.style.display = 'none';
+      if (textarea.value !== '') {
+        const tasksList = textarea.value.split(/\r?\n/).filter((item) => item);
+        checklist.lastCheckboxId = tasksList.length - 1;
+        tasksList.forEach((item, id) => {
+          const fieldset = checklist.createCheckbox(id);
+          const fieldsetTextarea = fieldset.querySelector('.checkbox-label__textarea');
+          fieldsetTextarea.value = `${item}`;
+          checklist.removeTaskByButton(fieldset);
+          checklist.removeEmptyTask(fieldset);
+          checklist.moveTaskField(fieldset);
+          checklist.calcTextareaHeight(fieldsetTextarea);
+          checklist.focusNewTaskField(fieldsetTextarea);
+          inputBox.append(fieldset);
+        });
+        textarea.value = null;
+      }
+      inputBox.append(checklist.newTaskField);
+      const newTaskFieldInput = checklist.newTaskField.querySelector('.adding-field__checkbox-textarea');
+      newTaskFieldInput.focus();
+      checklistButton.classList.add('pressed');
+      checklistButton.setAttribute('disabled', 'disabled');
+    });
+  },
+  init() {
+    this.createEditingBox();
+    this.changingToChecklist();
+  },
 };
 
-export default editingBox;
+export default editingBlock;
